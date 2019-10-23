38fdf42d80208a38fc89ed15cf669c92863c2bf6
louiema1n <louielcxy@live.cn>
2019-10-23 15:34:01 +0800

    新增 获取登录用户信息

--- src/main/java/com/louiemain/springsecuritydemo/controller/HomeController.java
+++ src/main/java/com/louiemain/springsecuritydemo/controller/HomeController.java
@@ -1,8 +1,12 @@
 package com.louiemain.springsecuritydemo.controller;
 
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import sun.plugin.liveconnect.SecurityContextHelper;
 
 /**
  * 主页
@@ -16,7 +20,14 @@ public class HomeController {
     }
 
     @GetMapping({"index","","/"})
-    public String index() {
+    public String index(Model model) {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if ("anonymousUser".equals(principal)) {
+            model.addAttribute("name", "anonymous");
+        } else {
+            User user = (User) principal;
+            model.addAttribute("name", user.getUsername());
+        }
         return "/index";
     }
 }
